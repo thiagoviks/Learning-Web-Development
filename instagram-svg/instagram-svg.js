8944145e24d375dc56b0da8ae83384d4f67b8a43
thiagosudgomes <62107168+thiagosudgomes@users.noreply.github.com>
2020-03-12 12:00:26 -0300

    Instagram-svg logo animation
    It's used anime js for create animation with svg.

---
+++ instagram-svg/instagram-svg.js
@@ -0,0 +1,8 @@
+anime({
+        targets: '#instagram-svg path',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutQuad',
+        duration: 5000,
+        direction: 'alternate',
+        loop: true
+      });
